71cc4d1498037d2079356e8b4244f0ed4472bcab
Eugene Belinski <eugeniub@gmail.com>
2015-04-25 19:33:00 -0500

    Working version

--- app.js
+++ app.js
@@ -3,17 +3,44 @@ $(document).ready(function() {
 
 	$("#hex-value").keyup(function() {
 		var rawValue = $("#hex-value").val();
+		var newValue = "";
 		// console.log("Value: " + rawValue);
 		
-		if (rawValue.length == 7 && rawValue[0] == '#') {
-			console.log("7 and #");
+		if (((rawValue.length == 7 || rawValue.length == 4) && rawValue[0] == '#') || ((rawValue.length == 6 || rawValue.length == 3) && rawValue[0] != '#')) {
+			if (rawValue[0] == '#') {
+				// console.log("7/4 and #");
+				newValue = rawValue.substring(1);
+			} else {
+				// console.log("6/3");
+				newValue = rawValue;
+			}
 
-			$("#objective-c-results").html("[UIColor colorWithRed:0.071 green:0.204 blue:0.337 alpha:1] /* "+rawValue+" */");
-		} else if (rawValue.length == 6 && rawValue[0] != '#') {
+			if (newValue.length == 3) {
+				newValue = newValue[0]+newValue[0]+newValue[1]+newValue[1]+newValue[2]+newValue[2];
+			}
 
+			var red = getColorValue(newValue[0]+newValue[1]);
+			var grn = getColorValue(newValue[2]+newValue[3]);
+			var blu = getColorValue(newValue[4]+newValue[5]);
+
+			if (isNaN(red) || isNaN(grn) || isNaN(blu)) {
+				$("#swift-results").html("");
+				$("#objective-c-results").html("");
+			} else {
+				$("#swift-results").html("UIColor(red: "+red+", green: "+grn+", blue: "+blu+", alpha: 1) /* #"+newValue+" */");
+				$("#objective-c-results").html("[UIColor colorWithRed:"+red+" green:"+grn+" blue:"+blu+" alpha:1]; /* #"+newValue+" */");
+			}
 		} else {
 			$("#swift-results").html("");
 			$("#objective-c-results").html("");
 		}
 	});
 });
+
+function getColorValue(hex) {
+	newValue = parseInt(hex, 16);
+	newValue = newValue / 255;
+	newValue = Math.round(newValue * 1000) / 1000;
+
+	return newValue;
+}
